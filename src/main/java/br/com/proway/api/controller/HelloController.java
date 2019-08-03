d231a9257c76b762ccf46ef7b1a7edb2021ab219
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2019-08-02 22:02:16 -0300

    Adicionando Controller

---
+++ src/main/java/br/com/proway/api/controller/HelloController.java
@@ -0,0 +1,18 @@
+package br.com.proway.api.controller;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@Path("hello")
+public class HelloController {
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getMensagem(@QueryParam("usuario") String usuario) {
+		return "Bem vindo: " + usuario;
+	}
+	
+}
